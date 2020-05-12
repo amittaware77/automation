84b88945e6ca44de18611ffd8813a534c33442a7
ataware <amittaware7@gmail.com>
2020-05-12 09:08:16 +0530

    Framework Enhancement

--- AutomationFrameworkDemo/src/test/java/com/cognizant/test/androidpages/BasePageAndroid.java
+++ AutomationFrameworkDemo/src/test/java/com/cognizant/test/androidpages/BasePageAndroid.java
@@ -21,34 +21,34 @@ import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 public class BasePageAndroid {
 
 	@AndroidFindBy(id = Config.APP_PCKAGE + ":id/signin_to_yourAccount")
-	public MobileElement signInToYourAccountTxt;
+	private MobileElement signInToYourAccountTxt;
 	
 	@AndroidFindBy(id = Config.APP_PCKAGE + ":id/sign_in_button")
-	public MobileElement signInBtn;
+	private MobileElement signInBtn;
 	
 	@AndroidFindBy(id = Config.APP_PCKAGE + ":id/rs_search_src_text")
-	public MobileElement searchTbx;
+	private MobileElement searchTbx;
 	
 	@AndroidFindBy(id = Config.APP_PCKAGE + ":id/search_right_cam_img")
-	public MobileElement cameraImg;
+	private MobileElement cameraImg;
 
 	@AndroidFindBy(className = "android.widget.EditText")
-	public MobileElement emailTxb;
+	private MobileElement emailTxb;
 
 	@AndroidFindBy(className = "android.widget.Button")
-	public MobileElement continueBtn;
+	private MobileElement continueBtn;
 	
 	@AndroidFindBy(className = "android.widget.EditText")
-	public MobileElement passwordTxb;
+	private MobileElement passwordTxb;
 	
 	@AndroidFindBy(className = "android.widget.Button")
-	public List<MobileElement> loginBtns;
+	private List<MobileElement> loginBtns;
 	
 	@AndroidFindBy(xpath = "//android.view.View[@text='Welcome']")
-	public MobileElement welcomeTxt;
+	private MobileElement welcomeTxt;
 	
 	@AndroidFindBy(xpath = "//android.view.View[@text='Login']")
-	public MobileElement loginTxt;
+	private MobileElement loginTxt;
 	
 	
 	public BasePageAndroid() {  
